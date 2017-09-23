54333ba3bed3b4b4aac53adb3fcb6765150b3c09
Michael Davis <rubiksmaster65@yahoo.com>
2017-09-23 17:32:44 -0400

    Show small names in small width
    This is because labels will collide with one another at small widths

--- personal/static/dist/me.js
+++ personal/static/dist/me.js
@@ -1,26 +1,6 @@
-const colors = {
-    "Brainfuck": "#2F2530",
-    "C": "#555555",
-    "CSS": "#563d7c",
-    "Go": "#375eab",
-    "HTML": "#e34c26",
-    "Java": "#b07219",
-    "JavaScript": "#f1e05a",
-    "Processing": "#0096D8",
-    "Python": "#3572A5",
-    "Ruby": "#701516",
-    "Shell": "#89e051",
-    "TeX": "#3D6117"
-};
-
 window.addEventListener('load', function () {
-    const svg = d3.select('svg');
-    if (!window.margin)
-        svg.on('click', function() {
-            window.location.href = "/language-graph"
-        });
-
-    const margin = window.margin || {left: 40, right: 20, top: 20, bottom: 30};
+    const svg = d3.select('svg'),
+        margin = window.margin || {left: 40, right: 20, top: 20, bottom: 30};
 
     let height = svg.node().getBoundingClientRect().width,
         width;
@@ -39,12 +19,14 @@ window.addEventListener('load', function () {
 
     d3.json('dist/linguae.json', function (data) {
 
-        const keys = Object.keys(data),
-            vals = Object.values(data),
-            largest = Math.max.apply(Math, vals);
+        const largest = Math.max.apply(Math, data.map(function (e) {
+                return e.time;
+            }));
 
         //  set the domains of the scales
-        x.domain(keys);
+        x.domain(data.map(function (e) {
+            return width > 500 ? e.name : e.alias || e.name;
+        }));
         y.domain([0, largest]);
 
         //  append the axes
@@ -59,17 +41,17 @@ window.addEventListener('load', function () {
 
         //  make the bars
         g.selectAll('rect.bar')
-            .data(keys)
+            .data(data)
             .enter()
             .append('rect')
             .classed('bar', true)
-            .attr('x', d => x(d))
-            .attr('y', d => y(data[d]))
+            .attr('x', d => x(width > 500 ? d.name : d.alias || d.name))
+            .attr('y', d => y(d.time))
             .attr('width', x.bandwidth())
-            .attr('height', d => height - y(data[d]))
+            .attr('height', d => height - y(d.time))
             .attr('stroke', '#000')
             .attr('stroke-width', 1)
-            .style('fill', d => colors[d] || "#000");
+            .style('fill', d => d.color || "#000");
 
     });
 });
