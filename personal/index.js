75dad1fa0a8ae43ff0a7dd9ec8c8563624628ae5
Michael Davis <rubiksmaster65@yahoo.com>
2017-09-25 20:24:37 -0400

    Add MongoDB ip database

--- personal/index.js
+++ personal/index.js
@@ -5,12 +5,33 @@ const express = require('express'),
     nun = require('nunjucks'),
     favicon = require('serve-favicon'),
     morgan = require('morgan'),
+    mongoose = require('mongoose'),
     app = express();
 
 nun.configure('templates', {
     autoescape: true,
     express: app
 });
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/traffic');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+    console.log('connected');
+});
+
+const trafficSchema = mongoose.Schema({
+    ip: String,
+    date: String,
+    method: String,
+    url: String,
+    httpVersion: Number,
+    status: String,
+    referrer: String,
+    userAgent: String
+}),
+    Traffic = mongoose.model('Traffic', trafficSchema);
 
 //  route the example sites
 app.use('/sites', sites);
@@ -33,6 +54,31 @@ morgan.token('ip', function (req, res) {
 });
 
 app.use(morgan(':ip - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
+
+//  write the traffic to the database
+app.use(function (req, res, next) {
+    const ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+
+    //  methods to get values borrowed from morgan (npm module)
+    const t = new Traffic({
+        ip: ip,
+        date: (new Date()).toUTCString(),
+        method: req.method,
+        url: req.originalUrl || req.url,
+        httpVersion: Number(req.httpVersionMajor + '.' + req.httpVersionMinor),
+        status: res._header ? String(res.statusCode) : undefined,
+        referrer: req.headers['referer'] || req.headers['referrer'],
+        userAgent: req.headers['user-agent']
+    });
+
+    t.save().catch(err => console.log('Failed to save to database'));
+
+    next();
+});
 
 app.get('/', function (req, res) {
     res.render('me.html');
