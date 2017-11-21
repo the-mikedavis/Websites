7efbcea3805b9e9ef045203584b2ccf51d47a22e
Michael Davis <rubiksmaster65@yahoo.com>
2017-11-21 12:34:07 -0800

    Add a section for explanation of the example

--- personal/content/ainfo.js
+++ personal/content/ainfo.js
@@ -2,26 +2,31 @@ module.exports = [
     {
         title: "Part I: Uninformed Search",
         link: "/projects/ai/uninformed",
-        content: "Uninformed Search is great for when you know very little about the solution to a problem or its environment."
+        content: "Uninformed Search is great for when you know very little about the solution to a problem or its environment.",
+        context: "This example is of the famous AI toy problem: Missionaries and Cannibals."
     },
     {
         title: "Part II: Informed Search",
         link: "/projects/ai/informed",
-        content: "Informed Search uses inferences about the solution to point the search in its direction."
+        content: "Informed Search uses inferences about the solution to point the search in its direction.",
+        context: ""
     },
     {
         title: "Part III: Genetic Algorithms",
         link: "/projects/ai/genetic",
-        content: "Genetic algorithms use randomness and 'survival of the fittest' strategies to find an answer to problems with many solutions."
+        content: "Genetic algorithms use randomness and 'survival of the fittest' strategies to find an answer to problems with many solutions.",
+        context: ""
     },
     {
         title: "Part IV: Constraint Satisfaction Problems",
         link: "/projects/ai/csp",
-        content: "Constraint Satisfaction Problems are everywhere. From solving airport schedules to 'four-coloring' the United States, CSPs make solving very complicated problems easy and fast."
+        content: "Constraint Satisfaction Problems are everywhere. From solving airport schedules to 'four-coloring' the United States, CSPs make solving very complicated problems easy and fast.",
+        context: ""
     },
     {
         title: "Part V: Adversarial Search",
         link: "/projects/ai/adversarial",
-        content: "Games like Tic-Tac-Toe and Chess use search much more than you might think. The strategies used in these games can help us solve problems like automated driving and diplomacy."
+        content: "Games like Tic-Tac-Toe and Chess use search much more than you might think. The strategies used in these games can help us solve problems like automated driving and diplomacy.",
+        context: ""
     }
 ];
