4a1e29eea51db335aec19bc19d442163412f36de
Michael Davis <rubiksmaster65@yahoo.com>
2017-11-21 12:26:20 -0800

    Template in the titles and descriptions for ai

--- personal/ai.js
+++ personal/ai.js
@@ -7,23 +7,23 @@ router.get('/', function (req, res) {
 });
 
 router.get('/uninformed', function (req, res) {
-    res.render('ai.html', { mod : "uninformed" });
+    res.render('ai.html', { mod : "uninformed", content : aicontent[0] });
 });
 
 router.get('/informed', function (req, res) {
-    res.render('ai.html', { mod : "local" });
+    res.render('ai.html', { mod : "local", content : aicontent[1] });
 });
 
 router.get('/genetic', function (req, res) {
-    res.render('ai.html', { mod : "genetic" });
+    res.render('ai.html', { mod : "genetic", content : aicontent[2] });
 });
 
 router.get('/csp', function (req, res) {
-    res.render('ai.html', { mod : "csp" });
+    res.render('ai.html', { mod : "csp", content: aicontent[3] });
 });
 
 router.get('/adversarial', function (req, res) {
-    res.render('ai.html', { mod : "adversarial" });
+    res.render('ai.html', { mod : "adversarial", content: aicontent[4] });
 });
 
 module.exports = router;
